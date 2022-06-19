11adffd51d799f15269997433935f5dc9a277e7a
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-06-19 11:29:06 +0530

    header file created

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const header = props => {
+  return (
+    <div>Kishan</div>
+  )
+}
+
+header.propTypes = {}
+
+export default header
