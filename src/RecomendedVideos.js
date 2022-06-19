5368ef7e7250ce911db14aacd1179db32245db91
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-06-19 19:07:07 +0530

    Recommended Videos

--- src/RecomendedVideos.js
+++ src/RecomendedVideos.js
@@ -1,9 +1,86 @@
 import React from 'react'
 import './RecomendedVideos.css'
+import VideoCard from './VideoCard'
+
 function RecomendedVideos() {
   return (
     <div className='recomendedVideos'>
-        <h2>This is where videos go</h2>
+        <h2>Recommended</h2>
+        <div className="recomendedVideos__videos">
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          <VideoCard
+            title="This is just a test"
+            views="10M"
+            timestamp="30 Minutes ago"
+            channelImage= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+            channel="UK volgs"
+            image= "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          />
+          
+        </div>
     </div>
   )
 }
