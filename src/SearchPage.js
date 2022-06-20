4b2516de40165192f2515a36d5f59019379cc9f4
Kishan-Kumar-Zalavadia <kishankumar3131@gmail.com>
2022-06-20 11:26:25 +0530

    Final

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './SearchPage.css'
 import ChannelRow from './ChannelRow';
+import VideoRow from './VideoRow';
 import TuneIcon from '@mui/icons-material/Tune';
 function SearchPage() {
   return (
@@ -18,6 +19,35 @@ function SearchPage() {
         subs="800K"
         noOfVideos={313}
         description="Test"
+      />
+      <hr/>
+
+      <VideoRow
+        views="2.3M"
+        subs="313K"
+        description="This is a dummy description"
+        timestamp="30 minutes ago"
+        channel="KU volgs"
+        title="This is a title"
+        image="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+      />
+      <VideoRow
+        views="2.3M"
+        subs="313K"
+        description="This is a dummy description"
+        timestamp="30 minutes ago"
+        channel="KU volgs"
+        title="This is a title"
+        image="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+      />
+      <VideoRow
+        views="2.3M"
+        subs="313K"
+        description="This is a dummy description"
+        timestamp="30 minutes ago"
+        channel="KU volgs"
+        title="This is a title"
+        image="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
       />
     </div>
   )
